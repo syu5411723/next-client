559101cfcb4e2e90fe81dec462fe64019382414a
maekawa-syuki <syu54117123>
2022-01-26 08:54:46 +0900

    prepared paths

---
+++ pages/api/[id].tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+export {Data} from "../../components/lib/data/Sec01Data"
+
+
+
+
+const DetailPage = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default DetailPage
