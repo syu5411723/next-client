a73fae152f78634c8d3910dc54eae386dd813fec
maekawa-syuki <syu54117123>
2022-01-25 10:08:08 +0900

    edit sec

--- components/atoms/main/home/Sec01/index.ts
+++ components/atoms/main/home/Sec01/index.ts
@@ -1,1 +1,4 @@
-﻿
+﻿export { Sec01Title } from './Sec01Title';
+export {Sec01Image} from './Sec01Image'
+export {Sec01Button} from './Sec01Button'
+export {Sec01Content} from './Sec01Content'
---
+++ components/molcules/main/home/Sec01.tsx
@@ -0,0 +1,23 @@
+﻿export {Sec01Title, Sec01Image, Sec01Button, Sec01Content} from "../../../atoms/main/home/Sec01/index"
+
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const RigghtWrapper = styled.div``
+const LeftWrapper = styled.div``
+
+export const Sec01 = () => {
+    return (
+        <>
+        <LeftWrapper>
+            <Sec01Title />
+            <Sec01Content />
+            <Sec01Button />
+        </LeftWrapper>
+        <RigghtWrapper>
+            <Sec01Image />
+        </RigghtWrapper>
+        </>
+    )
+}
