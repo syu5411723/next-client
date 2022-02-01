30dc471539d40ecc964e73216568ccb5fc3b379e
maekawa-syuki <syu54117123>
2022-02-02 07:43:03 +0900

    add section

--- lib/data/Sec01Data.ts
+++ lib/data/Sec01Data.ts
@@ -2,16 +2,16 @@
     {
         id: "1",
         title: "Home",
-        content: "tete",
-        button: "test",
+        content: "リビングを彩るアイテムで、お気に入りの空間を創ります。",
+        button: "Shop All Home",
         img: "/",
     },
     {
         id: "2",
-        title: "",
-        content: "",
-        button: "",
-        img: "",
+        title: "Kitchen & Dining",
+        content: "革新的なキッチンツールからダイニングルームを彩るアイテムまで、料理やスタイリングを楽しむことのできるアイテムが見つかります。",
+        button: "Shop All Kitchen & Dining",
+        img: "/",
 
     },
     // {
