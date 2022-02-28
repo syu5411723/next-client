de9e9fc5b74e03dcebe0082608a8575ccbe84dbe
maekawa-syuki <syu54117123>
2022-02-28 13:50:24 +0900

    add component

---
+++ components/design/atoms/middle/FooterMiddleHead.tsx
@@ -0,0 +1,17 @@
+﻿import {VFC} from 'react'
+import styled from 'styled-components'
+
+type Props = {
+    text: string
+}
+
+const Wrapper = styled.div``
+const Text = styled.p``
+
+export const FooterMiddleHead: VFC<Props> = ({text}) => {
+    return (
+        <Wrapper>
+            <Text>{text}</Text>
+        </Wrapper>
+    )
+}
