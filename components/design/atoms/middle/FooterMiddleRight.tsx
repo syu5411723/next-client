28a3a00d53fa19450f59ff10ee08bf1eb97393d1
maekawa-syuki <syu54117123>
2022-03-02 15:30:48 +0900

    add components

---
+++ components/design/atoms/middle/FooterMiddleRight.tsx
@@ -0,0 +1,21 @@
+﻿import styled from "styled-components"
+
+const Wrapper = styled.div``
+const Up = styled.p``
+const Middle = styled.p``
+const BottomWrapper = styled.div``
+const Buttom = styled.p``
+const Date = styled.p``
+
+export const FooterMiddleRight = () => {
+    return (
+        <Wrapper>
+            <Up>カスタマーセンター</Up>
+            <Middle>0120-977-459</Middle>
+            <BottomWrapper>
+                <Buttom></Buttom>
+                <Date></Date>
+            </BottomWrapper>
+        </Wrapper>
+    )
+}
