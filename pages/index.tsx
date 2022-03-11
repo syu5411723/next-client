ff08ac3ff6c3947c2fbab5393ef58eb2cbbc6ff7
maekawa-syuki <syu54117123>
2022-03-11 14:00:35 +0900

    add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Layout } from '../components/Layout/ Layout'
 import { Home } from '../components/pages/Home'
 
 const index = () => {
   return (
-    <>
+    <Layout>
       <Home />
-    </>
+    </Layout>
   )
 }
 
