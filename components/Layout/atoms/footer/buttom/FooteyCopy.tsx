c6f35827a28c6d926a457beb75e406951e867f4f
maekawa-syuki <syu54117123>
2022-03-17 15:29:16 +0900

    edit footer

---
+++ components/Layout/atoms/footer/buttom/FooteyCopy.tsx
@@ -0,0 +1,29 @@
+﻿import styled from "styled-components"
+
+const Container = styled.div``
+const UpWrapper = styled.div``
+const BottomWrapper = styled.div``
+const List = styled.div``
+
+const Item = styled.p``
+const CopyText = styled.p``
+const OfficialName = styled.p``
+
+const Datas = ["特定商法取引表示", "ご利用規約", "個人情報保護方針"]
+
+export const FooteyCopy = () => {
+    return (
+        <Container>
+            <UpWrapper>
+                <CopyText></CopyText>
+                <List>
+                    {Datas.map((item) => <Item>{item}</Item>)}
+                </List>
+            </UpWrapper>
+            <BottomWrapper>
+                <OfficialName></OfficialName>
+            </BottomWrapper>
+
+        </Container>
+    )
+}
