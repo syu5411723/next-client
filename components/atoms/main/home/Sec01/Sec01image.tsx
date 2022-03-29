62da7317228cdeaf9086b7dbeaaddabece1813ec
maekawa-syuki <syu54117123>
2022-03-29 11:41:12 +0900

    edit position

--- components/atoms/main/home/Sec01/Sec01image.tsx
+++ components/atoms/main/home/Sec01/Sec01image.tsx
@@ -5,13 +5,14 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
     width: 100px;
     height: 100px;
+    position: absolute;
 `
 
 export const Sec01Image = () => {
     return (
         <>
             <Wrapper>
-                {/* <Image src="/img/icon.jpg" layout='fill' objectFit='cover' /> */}
+                <Image src="/img/icon.jpg" layout='fill' objectFit='cover' />
             </Wrapper>
         </>
     )
