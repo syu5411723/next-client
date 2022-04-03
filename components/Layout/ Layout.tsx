44f593c6633e73b31aea8daf429ea6e7e711cd75
maekawa-syuki <syu54117123>
2022-04-03 11:40:41 +0900

    edit style

--- components/Layout/ Layout.tsx
+++ components/Layout/ Layout.tsx
@@ -1,5 +1,10 @@
-﻿import { Footer } from "./templates/Footer"
+﻿import styled from "styled-components"
+import { Footer } from "./templates/Footer"
 import { Header } from "./templates/Header"
+
+const Container = styled.div`
+    
+`
 
 
 export const Layout = ({ children }) => {
